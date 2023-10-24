51ea7a69361362dc24e5c422d38bc836ac0ca753
[SW5] 이수민 <tnklr1111@gmail.com>
2023-10-24 13:56:26 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.scss";
+import { Main } from "./pages/main/Main";
+import { CollectionInfo } from "./pages/collectionInfo/CollectionInfo";
+import { Commentary } from "./pages/commentary/Commentary";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/collection" element={<CollectionInfo />} />
+        <Route path="/commentary" element={<Commentary />} />
+        {/* <Route path="/story" element={} />
+        <Route path="/making" element={} />
+        <Route path="/quiz" element={} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/main/Main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./main.scss";
+
+export const Main = () => {
+  return <div className="title">Hello</div>;
+};
