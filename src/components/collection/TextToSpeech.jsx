8dfb46e0872cca44302ec358caae2e57ad0277a9
[SW5] 이수민 <tnklr1111@gmail.com>
2023-10-31 13:34:36 +0900

    fix: tts 끊김 현상 보완

--- src/components/collection/TextToSpeech.jsx
+++ src/components/collection/TextToSpeech.jsx
@@ -5,25 +5,57 @@ const TextToSpeech = ({ text }) => {
   const [voicesLoaded, setVoicesLoaded] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const splitTextIntoChunks = (text, chunkSize) => {
+    const chunks = [];
+    const sentences = text.text.match(/[^.!?]+[.!?]+/g);
+
+    if (sentences) {
+      sentences.forEach((sentence) => {
+        chunks.push(sentence);
+      });
+    } else {
+      chunks.push(text);
+    }
+    return chunks;
+  };
+
+  const speakChunks = (chunks) => {
+    const synth = window.speechSynthesis;
+
+    const speakChunk = (index) => {
+      if (index < chunks.length) {
+        const u = new SpeechSynthesisUtterance(chunks[index]);
+        u.rate = 1;
+
+        const voices = synth.getVoices();
+        if (voices.length) {
+          u.voice = voices.find((voice) => voice.name === "Google 한국의");
+        }
+
+        synth.speak(u);
+        u.onend = () => {
+          if (index === chunks.length - 1) {
+            setIsPlaying(false);
+          }
+          speakChunk(index + 1);
+        };
+      }
+    };
+
+    speakChunk(0);
+  };
+
   useEffect(() => {
     const synth = window.speechSynthesis;
-    const textWithoutParentheses = text.replace(/\([^()]*\)/g, "");
-    let u = new SpeechSynthesisUtterance(textWithoutParentheses);
+    const textWithoutParentheses = text.replace(
+      /\([^()]*\)|\[[^\]]*\]|[\p{Script=Han}]/gu,
+      ""
+    );
+    const u = new SpeechSynthesisUtterance(textWithoutParentheses);
     u.rate = 1;
-
-    // synth.onvoiceschanged = () => {
-    //   const voices = synth.getVoices();
-
-    //   if (voices.length) {
-    //     u.voice = voices[168];
-    //     setVoicesLoaded(true);
-    //     setUtterance(u);
-    //   }
-    // };
 
     const handleVoicesChanged = () => {
       const voices = synth.getVoices();
-      console.log(voices);
 
       if (voices.length) {
         u.voice = voices.find((voice) => voice.name === "Google 한국의");
@@ -31,6 +63,8 @@ const TextToSpeech = ({ text }) => {
         setUtterance(u);
       }
     };
+
+    handleVoicesChanged();
 
     synth.onvoiceschanged = handleVoicesChanged;
 
@@ -43,10 +77,11 @@ const TextToSpeech = ({ text }) => {
   const handleTogglePlay = () => {
     if (voicesLoaded && utterance) {
       const synth = window.speechSynthesis;
+      const chunks = splitTextIntoChunks(utterance, 15);
       if (isPlaying) {
         synth.cancel();
       } else {
-        synth.speak(utterance);
+        speakChunks(chunks);
       }
       setIsPlaying(!isPlaying);
     }
@@ -54,17 +89,19 @@ const TextToSpeech = ({ text }) => {
 
   return (
     <div style={{ paddingTop: "15%" }}>
-      <button onClick={handleTogglePlay} style={{ all: "unset" }}>
-        <img
-          src={
-            isPlaying
-              ? "/assets/image/soundOffBtn121.png"
-              : "/assets/image/soundOnBtn121.png"
-          }
-          alt={isPlaying ? "소리 끄기 버튼" : "소리 켜기 버튼"}
-          style={{ width: "50px", height: "50px" }}
-        />
-      </button>
+      {text ? (
+        <button onClick={handleTogglePlay} style={{ all: "unset" }}>
+          <img
+            src={
+              isPlaying
+                ? "/assets/image/soundOffBtn121.png"
+                : "/assets/image/soundOnBtn121.png"
+            }
+            alt={isPlaying ? "소리 끄기 버튼" : "소리 켜기 버튼"}
+            style={{ width: "50px", height: "50px" }}
+          />
+        </button>
+      ) : null}
     </div>
   );
 };
