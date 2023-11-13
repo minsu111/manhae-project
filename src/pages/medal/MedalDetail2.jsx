cdc335f979a762e28245b7f60ea3e8c15a268298
[SW5] 이수민 <tnklr1111@gmail.com>
2023-11-13 15:38:37 +0900

    훈장만들기 영문 번역본 추가 및 css 수정

--- src/pages/medal/MedalDetail2.jsx
+++ src/pages/medal/MedalDetail2.jsx
@@ -4,18 +4,41 @@ import { Link } from "react-router-dom";
 import "./medalDetail2.scss";
 
 export const MedalDetail2 = () => {
+  const language = sessionStorage.getItem("language");
+
   return (
-    <div className="medal_main_container">
-      <div className="section_wrapper2">
-        <div className="medal_detail2_seciton">
-          <h2>건국훈장 ‒ 대통령장(Presidential Medal)</h2>
-          <hr className="dividing_line" />
-          <p>
-            건국훈장에서 2등급인 대통령장에는 한글 대중화와 근대화 개척자 주시경
-            선생, 한국독립군 총사령관 지청천 장군, 한인애국단 이봉창 의사, 단재
-            신채호, 평민 의병장 신돌석, 종로경찰서에 폭탄을 투척했던 김상옥 열사
-            등의 위인분들이 수여 받았습니다.
-          </p>
+    <div className="medal_detail2_container">
+      <div className="detail2_section_wrapper">
+        {language === "Ko" ? (
+          <div className="medal_detail2_seciton">
+            <h2>건국훈장 ‒ 대통령장(Presidential Medal)</h2>
+            <hr className="dividing_line" />
+            <p>
+              건국훈장에서 2등급인 대통령장에는 한글 대중화와 근대화 개척자
+              주시경 선생, 한국독립군 총사령관 지청천 장군, 한인애국단 이봉창
+              의사, 단재 신채호, 평민 의병장 신돌석, 종로경찰서에 폭탄을
+              투척했던 김상옥 열사 등의 위인분들이 수여 받았습니다.
+            </p>
+          </div>
+        ) : (
+          <div className="medal_detail2_seciton">
+            <h2>Order of Merit for National Foundation - Presidential Medal</h2>
+            <hr className="dividing_line" />
+            <p>
+              Among the Order of Merit for National Foundation, the second grade
+              is the Presidential Medal, which has been awarded to distinguished
+              individuals such as Ju Si-gyeong, a pioneer in the popularization
+              and modernization of the Korean language; General Ji Cheong-cheon,
+              the commander-in-chief of the Korean Independence Army; patriot
+              Lee Bong-chang; historian Danjae Shin Chae-ho; commoner militia
+              leader Shin Dol-seok; and martyr Kim Sang-ok, who threw a bomb at
+              the Jongno Police Station.
+            </p>
+          </div>
+        )}
+
+        <div className="detail2_desc_img">
+          <img src={"/assets/medal/medal_detail2.png"} alt={"독립장"} />
         </div>
       </div>
     </div>
--- src/pages/medal/MedalDetail3.jsx
+++ src/pages/medal/MedalDetail3.jsx
@@ -4,35 +4,101 @@ import { Link } from "react-router-dom";
 import "./medalDetail3.scss";
 
 export const MedalDetail3 = () => {
+  const language = sessionStorage.getItem("language");
+
   return (
     <div className="medal_detail3_container">
-      <div className="section_wrapper3">
-        <div className="medal_detail3_seciton">
-          <h2>건국훈장 ‒ 독립장(Independence Medal)</h2>
-          <hr className="dividing_line" />
-          <p>
-            건국훈장에서 3등급인 독립장에는 여성독립운동가 김 마리아, 윤동주
-            시인, 우당 김희영, 제암리 사건을 전 세계에 알린 스코필드 박사등의
-            위인분들이 수여 받았습니다.
-          </p>
+      {language === "Ko" ? (
+        <div className="detail3_section_wrapper">
+          <div className="detail3_left_section">
+            <div className="medal_detail3_seciton">
+              <h2>건국훈장 ‒ 독립장(Independence Medal)</h2>
+              <hr className="dividing_line" />
+              <p>
+                건국훈장에서 3등급인 독립장에는 여성독립운동가 김 마리아, 윤동주
+                시인, 우당 김희영, 제암리 사건을 전 세계에 알린 스코필드
+                박사등의 위인분들이 수여 받았습니다.
+              </p>
+              <div className="detail3_desc_img_1">
+                <img src={"/assets/medal/medal_detail3_1.png"} alt={"독립장"} />
+              </div>
+            </div>
+          </div>
+          <div className="detail3_right_section">
+            <div className="medal_detail3_seciton">
+              <h2>건국훈장 ‒ 애국장(Patriotic Medal)</h2>
+              <hr className="dividing_line" />
+              <p>
+                건국훈장에서 4등급인 애국장에는 재야 대통령 장준하, 저항시인
+                이육사, 송몽규 등의 위인분들이 수여 받았습니다.
+              </p>
+              <div className="detail3_desc_img_2">
+                <img src={"/assets/medal/medal_detail3_2.png"} alt={"독립장"} />
+              </div>
+            </div>
+            <div className="medal_detail3_seciton detail3_section3">
+              <h2>건국훈장 ‒ 애족장(National Medal)</h2>
+              <hr className="dividing_line" />
+              <p>
+                건국훈장에서 5등급인 애족장에는 후세다쓰지, 여성 계몽을 이끈
+                차미리사 여사 등의 위인분들이 수여 받았습니다.
+              </p>
+              <div className="detail3_desc_img_2">
+                <img src={"/assets/medal/medal_detail3_2.png"} alt={"독립장"} />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="medal_detail3_seciton">
-          <h2>건국훈장 ‒ 애국장(Patriotic Medal)</h2>
-          <hr className="dividing_line" />
-          <p>
-            건국훈장에서 4등급인 애국장에는 재야 대통령 장준하, 저항시인 이육사,
-            송몽규 등의 위인분들이 수여 받았습니다.
-          </p>
+      ) : (
+        <div className="detail3_section_wrapper detail3_en">
+          <div className="detail3_left_section">
+            <div className="medal_detail3_seciton detail3_desc_en">
+              <h2>
+                Order of Merit for National Foundation - Independence Medal
+              </h2>
+              <hr className="dividing_line" />
+              <p>
+                The Independence Medal, which is the third grade in the Order of
+                Merit for National Foundation, has been awarded to prominent
+                individuals including female independence activist Kim Maria,
+                poet Yun Dong-ju, U-dang Kim Hee-young, and Dr. Schofield, who
+                brought the Jeam-ri massacre to the world's attention.
+              </p>
+              <div className="detail3_desc_img_1">
+                <img src={"/assets/medal/medal_detail3_1.png"} alt={"독립장"} />
+              </div>
+            </div>
+          </div>
+          <div className="detail3_right_section detail3_desc_en">
+            <div className="medal_detail3_seciton">
+              <h2>Order of Merit for National Foundation - Patriotic Medal</h2>
+              <hr className="dividing_line" />
+              <p>
+                The Patriotic Medal, which is the fourth grade in the Order of
+                Merit for National Foundation, has been awarded to distinguished
+                individuals such as Jang Jun-ha, known as the "President in the
+                Field," resistance poet Yi Yuk-sa, and Song Mong-gyu.
+              </p>
+              <div className="detail3_desc_img_2">
+                <img src={"/assets/medal/medal_detail3_2.png"} alt={"독립장"} />
+              </div>
+            </div>
+            <div className="medal_detail3_seciton detail3_section3 ">
+              <h2>Order of Merit for National Foundation - National Medal</h2>
+              <hr className="dividing_line" />
+              <p>
+                The National Medal, which is the fifth grade in the Order of
+                Merit for National Foundation, has been awarded to notable
+                individuals including Fusetsu Nakamura, and Madam Cha Mirisa,
+                who led the enlightenment for women.
+              </p>
+              <div className="detail3_desc_img_2">
+                <img src={"/assets/medal/medal_detail3_2.png"} alt={"독립장"} />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="medal_detail3_seciton">
-          <h2>건국훈장 ‒ 애족장(National Medal)</h2>
-          <hr className="dividing_line" />
-          <p>
-            건국훈장에서 5등급인 애족장에는 후세다쓰지, 여성 계몽을 이끈
-            차미리사 여사 등의 위인분들이 수여 받았습니다.
-          </p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
