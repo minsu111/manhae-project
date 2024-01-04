b6aebdb1ac53a638e7a1a2066113068ec3c71dc7
[SW5] 이수민 <tnklr1111@gmail.com>
2024-01-04 14:43:12 +0900

    feat: 퀴즈 점수 계산 로직 구현 및 만해이야기 데이터 추가

--- src/pages/quiz/QuizResult.jsx
+++ src/pages/quiz/QuizResult.jsx
@@ -4,14 +4,17 @@ import "./quizResult.scss";
 import TextZoomBar from "../../components/common/buttonBar/textZoom/TextZoomBar";
 
 const QuizResult = () => {
-  const score = sessionStorage.getItem("score");
+  const correctNum = sessionStorage.getItem("score");
   const baseFontSize = 1;
   const [fontSize, setFontSize] = useState(baseFontSize);
   const maxFontSize = baseFontSize + 0.4;
 
+  const score = ((correctNum / 15) * 100).toFixed();
+
   return (
-    <div style={{ fontSize: "100px", paddingLeft: "200px" }}>
-      맞힌 퀴즈 개수: {score}
+    <div style={{ fontSize: `${fontSize}vw`, paddingLeft: "200px" }}>
+      <p>맞힌 퀴즈 개수: {correctNum}</p>
+      <p>{score}점</p>
       <TextZoomBar
         textFontSize={fontSize}
         maxFontSize={maxFontSize}
