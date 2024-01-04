bab25e78fd4319552f7674917f606d8198f40854
[SW5] 이수민 <tnklr1111@gmail.com>
2024-01-04 20:58:25 +0900

    feat: 퀴즈 결과페이지 구현 완료

--- src/pages/quiz/QuizResult.jsx
+++ src/pages/quiz/QuizResult.jsx
@@ -5,16 +5,49 @@ import TextZoomBar from "../../components/common/buttonBar/textZoom/TextZoomBar"
 
 const QuizResult = () => {
   const correctNum = sessionStorage.getItem("score");
-  const baseFontSize = 1;
+  const baseFontSize = 3;
   const [fontSize, setFontSize] = useState(baseFontSize);
   const maxFontSize = baseFontSize + 0.4;
-
   const score = ((correctNum / 15) * 100).toFixed();
 
+  const language = sessionStorage.getItem("language");
+
   return (
-    <div style={{ fontSize: `${fontSize}vw`, paddingLeft: "200px" }}>
-      <p>맞힌 퀴즈 개수: {correctNum}</p>
-      <p>{score}점</p>
+    <div
+      className="quiz_result_container"
+      style={{
+        backgroundImage: `url("/assets/quiz/quiz_result_bg.jpeg")`,
+      }}
+    >
+      <div className="quiz_result_text_wrapper">
+        <img
+          src={`/assets/quiz/quiz_result_text_${language}.png`}
+          alt={"퀴즈풀이가 끝났습니다!"}
+        />
+      </div>
+      <div className="quiz_result_wrapper">
+        <div
+          className="quiz_result_wrong"
+          style={{ fontSize: `${fontSize}vw` }}
+        >
+          <div id="quiz_result_wrong_num">
+            <p>{15 - correctNum}</p>
+          </div>
+          <img src={"/assets/quiz/quiz_result_wrong.png"} alt={"틀린 개수"} />
+        </div>
+        <div
+          className="quiz_result_correct"
+          style={{ fontSize: `${fontSize}vw` }}
+        >
+          <div id="quiz_result_correct_num">
+            <p>{correctNum ? correctNum : 0}</p>
+          </div>
+          <img src={"/assets/quiz/quiz_result_correct.png"} alt={"맞힌 개수"} />
+        </div>
+      </div>
+      <div className="quiz_result_score_wrapper">
+        {language === "Ko" ? <p>{score} 점</p> : <p>{score} Points</p>}
+      </div>
       <TextZoomBar
         textFontSize={fontSize}
         maxFontSize={maxFontSize}
