14b9658785c2ce63b1c5df40f3bba1e463370ebb
이수민 <tnklr1111@gmail.com>
2024-09-03 13:43:22 +0900

    refactor: 훈장만들기 코드 개선
    - 중복되는 코드 개선
    - addImageToBoard 함수 분리
    - 메모이제이션 적용

--- src/pages/medal/MakingMedal.jsx
+++ src/pages/medal/MakingMedal.jsx
@@ -1,13 +1,19 @@
-import React, { useContext, useState } from "react";
-import { useDrop } from "react-dnd";
-import { LanguageContext } from "../../context/LanguageContext";
-import Object from "../../components/medal/Object";
-import TTSSpeaker from "../../components/common/speaker/TTSSpeaker";
-import TextZoomBar from "../../components/common/buttonBar/textZoom/TextZoomBar";
+import React, {
+  useContext,
+  useState,
+  useRef,
+  useMemo,
+  useCallback,
+} from 'react';
+import { useDrop } from 'react-dnd';
+import { LanguageContext } from '../../context/LanguageContext';
+import Object from '../../components/medal/Object';
+import TTSSpeaker from '../../components/common/speaker/TTSSpeaker';
+import TextZoomBar from '../../components/common/buttonBar/textZoom/TextZoomBar';
 
-import ObjectList from "../../data/MedalObjects";
+import ObjectList from '../../data/MedalObjects';
 
-import "./makingMedal.scss";
+import './makingMedal.scss';
 
 const MakingMedal = () => {
   const [medal, setMedal] = useState(null);
@@ -18,17 +24,34 @@ const MakingMedal = () => {
   const [fontSize, setFontSize] = useState(baseFontSize);
   const maxFontSize = baseFontSize + 0.4;
 
+  const boxRef = useRef();
+
   // 다국어 처리
   const { language } = useContext(LanguageContext);
 
-  const [subject, setSubject] = useState(
-    language === "Ko"
-      ? "아래 버튼을 눌러 주제를 선택해 주세요."
-      : "Please press the button below\nto select a topic."
+  const defaultSubject = useMemo(
+    () =>
+      language === 'Ko'
+        ? '아래 버튼을 눌러 주제를 선택해 주세요.'
+        : 'Please press the button below\nto select a topic.',
+    [language]
   );
 
+  const handleSubjectBtn = useCallback(
+    (e) => {
+      setSubject(
+        language === 'Ko'
+          ? `${e.target.value} 훈장 만들기`
+          : `Making '${e.target.value}' Medal`
+      );
+    },
+    [language]
+  );
+
+  const [subject, setSubject] = useState(defaultSubject);
+
   const [, drop] = useDrop(() => ({
-    accept: "image",
+    accept: 'image',
     drop: (medalItem, monitor) => addImageToBoard(medalItem, monitor),
     collect: (monitor) => ({
       isOver: !!monitor.isOver(),
@@ -36,210 +59,162 @@ const MakingMedal = () => {
   }));
 
   const addImageToBoard = (medalItem, monitor) => {
-    const id = medalItem.id;
-    const index = medalItem.index;
-    const objectList = ObjectList.filter((object) => id === object.id);
-    const item = objectList[0];
+    const { id, index } = medalItem;
+    const item = ObjectList.find((object) => id === object.id);
 
-    switch (item.groupId) {
-      case 1:
-        setMedal(item);
-        break;
-      case 2:
-        setMiddleRibon(item);
-        break;
-      case 3:
-        setBottomRibon(item);
-        break;
-      case 4:
-        const imageboxPos = document
-          .getElementById("imagebox")
-          .getBoundingClientRect();
-        const dragStartPos = monitor.getInitialClientOffset();
-        const itemPos = monitor.getClientOffset();
+    if (!item) return;
 
-        if (imageboxPos.x < dragStartPos.x && imageboxPos.y < dragStartPos.y) {
-          // 박스안에서 드래그시작일 경우 기존 아이템 유지
-          setExtraItems((prevExtraItems) => {
-            const updatedItems = prevExtraItems.map((object) => {
-              if (id === object.id && index === object.index) {
-                return {
-                  ...object,
-                  left: itemPos.x - imageboxPos.x,
-                  top: itemPos.y - imageboxPos.y,
-                };
-              }
-              return object;
-            });
-            return updatedItems;
-          });
-        } else {
-          // 새로운 아이템 추가
-          setExtraItems((prevExtraItems) => [
-            ...prevExtraItems,
-            {
-              ...item,
-              index: prevExtraItems.length + 1,
-              left: itemPos.x - imageboxPos.x,
-              top: itemPos.y - imageboxPos.y,
-            },
-          ]);
-        }
-
-        break;
-      default:
-        break;
+    if (item.groupId === 1) {
+      setMedal(item);
+    } else if (item.groupId === 2) {
+      setMiddleRibon(item);
+    } else if (item.groupId === 3) {
+      setBottomRibon(item);
+    } else if (item.groupId === 4) {
+      handleExtraItemDrop(item, monitor, id, index);
     }
   };
 
-  const handleSubjectBtn = (e) => {
-    language === "Ko"
-      ? setSubject(e.target.value + " 훈장 만들기")
-      : setSubject(`Making '${e.target.value}' Medal`);
+  const handleExtraItemDrop = (item, monitor, id, index) => {
+    const imageboxPos = boxRef.current.getBoundingClientRect();
+    const dragStartPos = monitor.getInitialClientOffset();
+    const itemPos = monitor.getClientOffset();
+
+    if (imageboxPos.x < dragStartPos.x && imageboxPos.y < dragStartPos.y) {
+      updateExtraItemPos(id, index, itemPos, imageboxPos);
+    } else {
+      addNewExtraItem(item, itemPos, imageboxPos);
+    }
+  };
+
+  const updateExtraItemPos = (id, index, itemPos, imageboxPos) => {
+    setExtraItems((prevItems) =>
+      prevItems.map((obj) =>
+        id === obj.id && index === obj.index
+          ? {
+              ...obj,
+              left: itemPos.x - imageboxPos.x,
+              top: itemPos.y - imageboxPos.y,
+            }
+          : obj
+      )
+    );
+  };
+
+  const addNewExtraItem = (item, itemPos, imageboxPos) => {
+    setExtraItems((prevItems) => [
+      ...prevItems,
+      {
+        ...item,
+        index: prevItems.length + 1,
+        left: itemPos.x - imageboxPos.x,
+        top: itemPos.y - imageboxPos.y,
+      },
+    ]);
+  };
+
+  const renderObjects = useCallback((groupId) => {
+    return ObjectList.filter((object) => object.groupId === groupId).map(
+      (object) => (
+        <div key={object.id} className={`medal_objects_group${groupId}`}>
+          <Object medalItem={object} />
+        </div>
+      )
+    );
+  }, []);
+
+  const renderObjectInBoard = (object, zIndex) =>
+    object && (
+      <div
+        style={{
+          position: 'absolute',
+          top: `${object.top}%`,
+          left: `${object.left}%`,
+          width: `${object.width}vw`,
+          zIndex: zIndex || '0',
+        }}
+      >
+        <Object medalItem={object} />
+      </div>
+    );
+
+  const renderButtons = () => {
+    const buttonLabels =
+      language === 'Ko'
+        ? ['애국심', '건강', '정직', '성실', '효도']
+        : ['Patriotism', 'Health', 'Honesty', 'Diligence', 'Filial Piety'];
+
+    return (
+      <div className='medal_btn_section'>
+        {buttonLabels.map((label) => (
+          <input
+            key={label}
+            type='button'
+            className={`${subject.includes(label) && 'double_height'}`}
+            value={label}
+            onClick={handleSubjectBtn}
+          />
+        ))}
+      </div>
+    );
   };
 
   return (
     <div
-      className="making_medal_container"
+      className='making_medal_container'
       style={{ fontSize: `${fontSize}vw` }}
     >
-      <div className="making_medal_section_wrapper">
-        <div className="left_section">
-          {language === "Ko" ? (
-            <div className="making_description">
-              <h1>나만의 훈장 만들기</h1>
-              <hr />
-              <p>
-                대한민국 훈장과 만해 한용운 선생님의 훈장을 참고하여 나만의
-                훈장을 만들어볼까요? 오른쪽 네모 박스에 원하는 요소들을 가져와서
-                나만의 훈장을 완성한 후, 사진을 찍어 가족, 친구들과
-                공유해보세요!
-              </p>
-            </div>
-          ) : (
-            <div className="making_description">
-              <h1>Create Your Own Medal!</h1>
-              <hr />
-              <p>
-                Let’s create our own medals referring to the Orders of the
-                Republic of Korea and the medals of Master Manhae Han Yong-un!
-                Bring the elements you want into the right square box to
-                complete your own medal, then take a picture and share it with
-                your family and friends!
-              </p>
-            </div>
-          )}
-
-          <div className="objects_wrapper">
-            <div className="objects1">
-              {ObjectList.map((object) => {
-                return (
-                  object.groupId === 1 && (
-                    <div className="medal_objects_group1">
-                      <Object medalItem={object} />
-                    </div>
-                  )
-                );
-              })}
-            </div>
+      <div className='making_medal_section_wrapper'>
+        <div className='left_section'>
+          <div className='making_description'>
+            <h1>
+              {language === 'Ko'
+                ? '나만의 훈장 만들기'
+                : 'Create Your Own Medal!'}
+            </h1>
             <hr />
-            <div className="objects2">
-              {ObjectList.map((object) => {
-                return (
-                  object.groupId === 2 && (
-                    <div className="medal_objects_group2">
-                      <Object medalItem={object} />
-                    </div>
-                  )
-                );
-              })}
-            </div>
-            <div className="objects3">
-              {ObjectList.map((object) => {
-                return (
-                  object.groupId === 3 && (
-                    <div className="medal_objects_group3">
-                      <Object medalItem={object} />
-                    </div>
-                  )
-                );
-              })}
-            </div>
+            <p>
+              {language === 'Ko'
+                ? '대한민국 훈장과 만해 한용운 선생님의 훈장을 참고하여 나만의 훈장을 만들어볼까요? 오른쪽 네모 박스에 원하는 요소들을 가져와서 나만의 훈장을 완성한 후, 사진을 찍어 가족, 친구들과 공유해보세요!'
+                : 'Let’s create our own medals referring to the Orders of the Republic of Korea and the medals of Master Manhae Han Yong-un! Bring the elements you want into the right square box to complete your own medal, then take a picture and share it with your family and friends!'}
+            </p>
+          </div>
+          <div className='objects_wrapper'>
+            <div className='objects1'>{renderObjects(1)}</div>
             <hr />
-            <div className="objects4">
-              {ObjectList.map((object) => {
-                return (
-                  object.groupId === 4 && (
-                    <div className="medal_objects_group4">
-                      <Object medalItem={object} />
-                    </div>
-                  )
-                );
-              })}
-            </div>
+            <div className='objects2'>{renderObjects(2)}</div>
+            <div className='objects3'>{renderObjects(3)}</div>
+            <hr />
+            <div className='objects4'>{renderObjects(4)}</div>
           </div>
         </div>
         {/* board 섹션 */}
-        <div className="right_section">
-          <div className="board_top_object">
+        <div className='right_section'>
+          <div className='board_top_object'>
             <img
-              src={"/assets/medal/medal_board_object.png"}
-              alt={"훈장 만들기 보드"}
+              src={'/assets/medal/medal_board_object.png'}
+              alt={'훈장 만들기 보드'}
             />
           </div>
-          <div className="boards" id="imagebox">
-            <div className="board_title">
+          <div className='boards' id='imagebox' ref={boxRef}>
+            <div className='board_title'>
               <p>{subject}</p>
             </div>
-            <div className="drop_section" ref={drop}>
-              {medal !== null && (
-                <div
-                  style={{
-                    position: "absolute",
-                    top: `${medal.top}vh`,
-                    left: `${medal.left}vw`,
-                    width: `${medal.width}vw`,
-                    zIndex: "3",
-                  }}
-                >
-                  <Object medalItem={medal} />
-                </div>
-              )}
-              {middleRibon !== null && (
-                <div
-                  style={{
-                    position: "absolute",
-                    top: `${middleRibon.top}%`,
-                    left: `${middleRibon.left}%`,
-                    width: `${middleRibon.width}vw`,
-                  }}
-                >
-                  <Object medalItem={middleRibon} />
-                </div>
-              )}
-              {bottomRibon !== null && (
-                <div
-                  style={{
-                    position: "absolute",
-                    top: `${bottomRibon.top}%`,
-                    left: `${bottomRibon.left}%`,
-                    width: `${bottomRibon.width}vw`,
-                    zIndex: "2",
-                  }}
-                >
-                  <Object medalItem={bottomRibon} />
-                </div>
-              )}
+            <div className='drop_section' ref={drop}>
+              {renderObjectInBoard(medal, 3)}
+              {renderObjectInBoard(middleRibon)}
+              {renderObjectInBoard(bottomRibon, 2)}
+
               {extraItems.map((object) => {
                 return (
                   <div
                     key={object.id}
                     style={{
-                      position: "absolute",
+                      position: 'absolute',
                       left: `calc(${object.left}px - ${object.width / 2}vw)`,
                       top: `calc(${object.top}px - ${object.width / 2}vw)`,
                       width: `${object.width}vw`,
-                      zIndex: "4",
+                      zIndex: '4',
                     }}
                   >
                     <Object medalItem={object} />
@@ -247,84 +222,10 @@ const MakingMedal = () => {
                 );
               })}
             </div>
-            {language === "Ko" ? (
-              <div className="medal_btn_section">
-                <input
-                  type="button"
-                  className={`${subject.includes("애국심") && "double_height"}`}
-                  value="애국심"
-                  onClick={handleSubjectBtn}
-                />
-                <input
-                  type="button"
-                  className={`${subject.includes("건강") && "double_height"}`}
-                  value="건강"
-                  onClick={handleSubjectBtn}
-                />
-                <input
-                  type="button"
-                  className={`${subject.includes("정직") && "double_height"}`}
-                  value="정직"
-                  onClick={handleSubjectBtn}
-                />
-                <input
-                  type="button"
-                  className={`${subject.includes("성실") && "double_height"}`}
-                  value="성실"
-                  onClick={handleSubjectBtn}
-                />
-                <input
-                  type="button"
-                  className={`${subject.includes("효도") && "double_height"}`}
-                  value="효도"
-                  onClick={handleSubjectBtn}
-                />
-              </div>
-            ) : (
-              <div className="medal_btn_section">
-                <input
-                  type="button"
-                  className={`${
-                    subject.includes("Patriotism") && "double_height"
-                  }`}
-                  value="Patriotism"
-                  onClick={handleSubjectBtn}
-                />
-                <input
-                  type="button"
-                  className={`${subject.includes("Health") && "double_height"}`}
-                  value="Health"
-                  onClick={handleSubjectBtn}
-                />
-                <input
-                  type="button"
-                  className={`${
-                    subject.includes("Honesty") && "double_height"
-                  }`}
-                  value="Honesty"
-                  onClick={handleSubjectBtn}
-                />
-                <input
-                  type="button"
-                  className={`${
-                    subject.includes("Diligence") && "double_height"
-                  }`}
-                  value="Diligence"
-                  onClick={handleSubjectBtn}
-                />
-                <input
-                  type="button"
-                  className={`${
-                    subject.includes("Filial Piety") && "double_height"
-                  }`}
-                  value="Filial Piety"
-                  onClick={handleSubjectBtn}
-                />
-              </div>
-            )}
+            {renderButtons()}
           </div>
           <div
-            className="refresh_btn"
+            className='refresh_btn'
             onClick={() => {
               setMedal(null);
               setMiddleRibon(null);
@@ -332,11 +233,11 @@ const MakingMedal = () => {
               setExtraItems([]);
             }}
           >
-            <img src={"/assets/medal/refresh_btn.png"} alt={"다시 만들기"} />
+            <img src={'/assets/medal/refresh_btn.png'} alt={'다시 만들기'} />
           </div>
         </div>
       </div>
-      <div className="play_btn">
+      <div className='play_btn'>
         <TTSSpeaker />
       </div>
       <TextZoomBar
